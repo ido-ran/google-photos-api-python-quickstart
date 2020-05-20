c635e929d8ab7724d158407fb677b38b3b898aa0
Grant Geyer <GrantG012@yahoo.com>
2020-05-20 18:00:43 -0400

    uses current, not-deprecated libraries

--- quickstart.py
+++ quickstart.py
@@ -5,18 +5,27 @@ Creates a Photos v1 API service and prints the names and ids of the last 10 albu
 the user has access to.
 """
 from __future__ import print_function
-from apiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+import json
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+import google_auth_httplib2  # This gotta be installed for build() to work
 
 # Setup the Photo v1 API
 SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly']
-store = file.Storage('credentials.json')
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-    creds = tools.run_flow(flow, store)
-service = build('photoslibrary', 'v1', http=creds.authorize(Http()))
+creds = None
+if(os.path.exists("token.pickle")):
+    with open("token.pickle", "rb") as tokenFile:
+        creds = pickle.load(tokenFile)
+if not creds or not creds.valid:
+    if (creds and creds.expired and creds.refresh_token):
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
+        creds = flow.run_local_server(port = 0)
+    with open("token.pickle", "wb") as tokenFile:
+        pickle.dump(creds, tokenFile)
+service = build('photoslibrary', 'v1', credentials = creds)
 
 # Call the Photo v1 API
 results = service.albums().list(
